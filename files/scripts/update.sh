fb5951377ff6d22ce78e81caa5b98001f07890aa
UO232627 - Alejandro Álvarez Solís <91067496+UO232627@users.noreply.github.com>
2022-08-04 12:55:31 +0200

    Update update.sh

--- files/scripts/update.sh
+++ files/scripts/update.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 sudo apt update -y
 sudo apt full-upgrade -y
-sudo reboot
+
+sudo echo "Actualización completada. El sistema se reiniciará en breves segundos..."
+sleep 5
+
+sudo systemctl reboot
